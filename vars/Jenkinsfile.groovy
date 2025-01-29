3cf9b7c5e867bab2219f5d479fe4ae3ffc5124d7
Prem <prem@mobilesentrix.com>
2025-01-29 19:01:40 +0530

    test commit

--- vars/Jenkinsfile.groovy
+++ vars/Jenkinsfile.groovy
@@ -1,25 +1,17 @@
 def call(Map pipelineParams) {
     stage('Checkout') {
-        steps {
-            git branch: pipelineParams.branch, url: pipelineParams.scmUrl
-        }
+        git branch: pipelineParams.branch, url: pipelineParams.scmUrl
     }
 
     stage('Build') {
-        steps {
-            sh 'echo Building project...'
-        }
+        sh 'echo Building project...'
     }
 
     stage('Test') {
-        steps {
-            sh 'echo Running tests...'
-        }
+        sh 'echo Running tests...'
     }
 
     stage('Deploy') {
-        steps {
-            sh "echo Deploying to ${pipelineParams.developmentServer}:${pipelineParams.serverPort}"
-        }
+        sh "echo Deploying to ${pipelineParams.developmentServer}:${pipelineParams.serverPort}"
     }
 }
